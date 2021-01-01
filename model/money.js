3839e617c37219aee5436d657a4fd2828a75c7f7
Dylan-Pro <71563237+Dylan-Pro@users.noreply.github.com>
2021-01-01 11:14:39 -0300

    Delete money.js

--- model/money.js
+++
@@ -1,7 +0,0 @@
-const mongoose = require("mongoose");
-const moneySchema = new mongoose.Schema({
-  userId: { type: String },
-  dinero: { type: Number },
-  guildId: { type: String }
-});
-module.exports = mongoose.model("Money", moneySchema);
